d02ba4ff6dfae1678c43a8dd7d5afa0b882185cc
Rishabh Saxena <40590709+saxena-rishabh@users.noreply.github.com>
2022-10-13 15:53:56 +0530

    updated s3ToTextract

--- src/s3ToTextract.py
+++ src/s3ToTextract.py
@@ -18,8 +18,8 @@ def get_kv_map(obj, bucket):
             Document={
                 # 'Bytes': b'bytes',
                 'S3Object': {
-                    'Bucket': 'awshackathonoct2023',
-                    'Name': 'DIG1.png'
+                    'Bucket': bucket,
+                    'Name': obj
                     }
             }
             )
@@ -53,65 +53,17 @@ def get_kv_map(obj, bucket):
     except:
         print("Failed while parsing the document")
         raise 
-    
-    
-def get_kv_relationship(key_map, value_map, block_map):
-    kvs = {}
-    for block_id, key_block in key_map.items():
-        value_block = find_value_block(key_block, value_map)
-        key = get_text(key_block, block_map)
-        val = get_text(value_block, block_map)
-        kvs[key] = val
-    print("kvs",kvs)
-    return kvs
-
-
-def find_value_block(key_block, value_map):
-    for relationship in key_block['Relationships']:
-        if relationship['Type'] == 'VALUE':
-            for value_id in relationship['Ids']:
-                value_block = value_map[value_id]
-    return value_block
-
-
-def get_text(result, blocks_map):
-    text = ''
-    if 'Relationships' in result:
-        for relationship in result['Relationships']:
-            if relationship['Type'] == 'CHILD':
-                for child_id in relationship['Ids']:
-                    word = blocks_map[child_id]
-                    if word['BlockType'] == 'WORD':
-                        text += word['Text'] + ' '
-                    if word['BlockType'] == 'SELECTION_ELEMENT':
-                        if word['SelectionStatus'] == 'SELECTED':
-                            text += 'X '
-
-
-    return text
-
-
-def print_kvs(kvs):
-    for key, value in kvs.items():
-        print(key, ":", value)
-
-
-def search_value(kvs, search_key):
-    for key, value in kvs.items():
-        if re.search(search_key, key, re.IGNORECASE):
-            return value
 
 
 def lambda_handler(event, context):
     
     # This code gets triggered by a object upload in S3 Bucket.
     # It sends object from S3 bucket to textract for key-value extracttion.
-    # In successful extraction it stores key-value pairs in S3 as CSV file.
+    # In successful extraction it stores dara line by line in S3 as text file.
     # In case of failures it prompts message.
     
     print(event)
     obj=event['Records'][0]['s3']['object']['key']
-    print(obj)
     bucket=event['Records'][0]['s3']['bucket']['name']
     
     try:
@@ -120,17 +72,11 @@ def lambda_handler(event, context):
         print("value_map",value_map)
         print("block_map",block_map)
         print("line_val",line_val)
-        # Get Key Value relationship
-        kvs = get_kv_relationship(key_map, value_map, block_map)
-        kvs['PROCEDURE'] = line_val
-        print("\n\n== FOUND KEY : VALUE pairs ===\n")
-        print_kvs(kvs)
     
         # Add to Queue for validation
-        msg = sqs_client.send_message(QueueUrl='textractToValidate.fifo',MessageBody=str(line_val),MessageGroupId="678Abc",
+        msg = sqs_client.send_message(QueueUrl='textractToValidate.fifo',MessageBody=str(line_val),MessageGroupId=line_val[0],
         MessageDeduplicationId=str(uuid.uuid4()))
 
-        print("msg",msg)
     except Exception as e:
         print("Failed in processing file")
         print(e)
