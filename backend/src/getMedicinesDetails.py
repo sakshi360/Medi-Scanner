a1c69f9f0a1bf86f27beec6d3f92247a51d8fff2
Sakshi Agarwal <36631361+sakshi360@users.noreply.github.com>
2022-10-26 17:28:22 +0530

    updated lambda with powertools

--- backend/src/getMedicinesDetails.py
+++ backend/src/getMedicinesDetails.py
@@ -5,17 +5,24 @@ from io import StringIO
 from decimal import Decimal
 import time
 import os
+from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
+
 dynamodb=boto3.resource("dynamodb")
 TableForMedicalEntryLedger = os.environ.get("MEDICAL_ENTRY_LEDGER")
 tableUserEntry=dynamodb.Table(TableForMedicalEntryLedger)
 username = os.environ.get("USERNAME")
 
+
+@metrics.log_metrics(capture_cold_start_metric=True)
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
     # TODO implement
     res = tableUserEntry.query(TableName= TableForMedicalEntryLedger,
             KeyConditionExpression=Key("username").eq(username), ScanIndexForward=False)
                             
-    print("result",res)
+    logger.info("result is ".format(res))
     if res == None or 'Items' not in res or len(res['Items']) == 0 :
         print("result is empty")
         return{
