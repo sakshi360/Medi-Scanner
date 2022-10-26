376c9b4899e41cd666ba29005f7a71280e55e8a4
Sakshi Agarwal <36631361+sakshi360@users.noreply.github.com>
2022-10-26 17:34:07 +0530

    updated powertools in lambda

--- backend/src/powertools/getMedicinesDetails.py
+++ backend/src/powertools/getMedicinesDetails.py
@@ -7,29 +7,27 @@ import time
 import os
 from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
+from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEventV2
 
 dynamodb=boto3.resource("dynamodb")
 TableForMedicalEntryLedger = os.environ.get("MEDICAL_ENTRY_LEDGER")
 tableUserEntry=dynamodb.Table(TableForMedicalEntryLedger)
 username = os.environ.get("USERNAME")
 
-
 @metrics.log_metrics(capture_cold_start_metric=True)
 @logger.inject_lambda_context
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
     # TODO implement
-    res = tableUserEntry.query(TableName= TableForMedicalEntryLedger,
-            KeyConditionExpression=Key("username").eq(username), ScanIndexForward=False)
-                            
+    logger.info("event is".format(event))
+    res = tableUserEntry.query(TableName= TableForMedicalEntryLedger,KeyConditionExpression=Key("username").eq(username), ScanIndexForward=False)                        
     logger.info("result is ".format(res))
     if res == None or 'Items' not in res or len(res['Items']) == 0 :
-        print("result is empty")
+        logger.info("result is empty")
         return{
             'statusCode':404,
             'body': json.dumps('Something went wrong!!')
         }
-    
     return {
         'statusCode': 200,
         'body': json.dumps({'medicines' : res['Items'][0]['medication']})
